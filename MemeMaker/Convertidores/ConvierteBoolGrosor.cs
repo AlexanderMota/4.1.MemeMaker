3be56ae1d9c066952b39a693fe7e8143e3dfa669
Alexander Mota <AlexanderMota@outlook.es>
2021-11-02 15:06:56 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/Convertidores/ConvierteBoolGrosor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker.Convertidores
+{
+    class ConvierteBoolGrosor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => (bool)value ? 0 : 5;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => (bool)value ? 5 : 0;
+    }
+}
